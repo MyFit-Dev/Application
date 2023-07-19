ad4e4f85137efb12f9f0ae3f7f984502b4d2822a
AEndrix <38986519+AEndrix03@users.noreply.github.com>
2023-07-19 04:14:37 +0200

    Aggunti Font

--- app/src/main/java/com/myfit/HomepageActivity.kt
+++ app/src/main/java/com/myfit/HomepageActivity.kt
@@ -17,21 +17,35 @@ class HomepageActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_homepage)
 
+        setupChart(2000, 0, 0)
+    }
+
+    fun setupChart(goal: Int, food: Int, exercise: Int) {
+        val rest = goal - food;
+
+        //goal : 100 = food : x
+        val percFood: Float = ((food * 100) / goal).toFloat()
+        val percExercise: Float = (((exercise - food) * 100) / (goal)).toFloat()
+
         pieChart = findViewById(R.id.pieChart)
 
         // Crea un ArrayList di PieEntry per i dati del grafico
         val entries = ArrayList<PieEntry>()
-        entries.add(PieEntry(30f, "Proteine"))
-        entries.add(PieEntry(20f, "Grassi"))
-        entries.add(PieEntry(50f, "Carboidrati"))
+        entries.add(PieEntry(percExercise, "Esercizio"))
+        entries.add(PieEntry(percFood, "Alimenti"))
+
+        if (rest > 0)
+            entries.add(PieEntry((1-percFood), "Resto"))
+        else
+            entries.add(PieEntry(0f, "Resto"))
+
+        entries.add(PieEntry(10f, "Resto"))
 
         // Crea un PieDataSet con gli entry
         val dataSet = PieDataSet(entries, "Label")
 
         // Opzioni per il dataset
-        dataSet.colors = listOf(Color.RED, Color.BLUE, Color.rgb(255, 165, 0))
-        dataSet.valueTextColor = Color.WHITE
-        dataSet.valueTextSize = 14f
+        dataSet.colors = listOf(Color.rgb(219,76,64), Color.rgb(60, 185, 183), Color.rgb(226, 226, 226))
 
         // Crea un PieData oggetto con il dataSet
         val data = PieData(dataSet)
